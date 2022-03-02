bec14b9bbf44100e6cb3e8affd82fdabefac226d
RobertS <servadorobert@gmail.com>
2022-03-01 20:53:41 -0500

    Some Hello World and name strin concatenate function examples

---
+++ hello.js
@@ -0,0 +1,14 @@
+/*const sayHello = function(){
+  console.log("Hello, World");
+}
+
+sayHello();*/
+
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+};
+
+//sayHello("Robert");
+//sayHello("Caliban");
+//sayHello("Miranda");
+//sayHello("Ferdinand");
