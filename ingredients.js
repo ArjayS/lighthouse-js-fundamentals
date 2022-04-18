0bab34ba824b6eb7da00381f3d591eb5fafdcc64
Robert Servado <servadorobertgmail.com>
2022-04-17 21:58:44 -0400

    Using For Loops on Banana Bread ingredients array

--- ingredients.js
+++ ingredients.js
@@ -9,10 +9,15 @@ const ingredients = [
   "banana",
 ];
 
-//While Loop on Banana Bread Ingrdients Array
+// //While Loop on Banana Bread Ingrdients Array
+// console.log("Banana Bread ingredients:");
+// let i = 0;
+// while (i < ingredients.length) {
+//   console.log(ingredients[i]);
+//   i++;
+// }
+
 console.log("Banana Bread ingredients:");
-let i = 0;
-while (i < ingredients.length) {
+for (i = 0; i < ingredients.length; i++) {
   console.log(ingredients[i]);
-  i++;
 }
