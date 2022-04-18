f84b569f7ffe42519cb39f2e54238af5ebdc1af0
Robert Servado <servadorobertgmail.com>
2022-04-17 21:55:28 -0400

    Using While Loops for Banana Bread ingredients array

---
+++ ingredients.js
@@ -0,0 +1,18 @@
+const ingredients = [
+  "eggs",
+  "milk",
+  "flour",
+  "sugar",
+  "baking soda",
+  "baking powder",
+  "chocolate chips",
+  "banana",
+];
+
+//While Loop on Banana Bread Ingrdients Array
+console.log("Banana Bread ingredients:");
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
